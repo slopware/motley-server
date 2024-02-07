1c3a39bc239e7362250d5b21671bc30e9bc92316
slopware <travisculbreth@gmail.com>
2024-02-07 12:14:37 -0500

    added new streaming server and playback techniques

---
+++ server_test.py
@@ -0,0 +1,103 @@
+import io
+import os
+import base64
+import torch
+import torchaudio
+import wave
+import numpy as np
+
+from fastapi import FastAPI, UploadFile, Body
+from fastapi.responses import StreamingResponse
+
+from pydantic import BaseModel
+#from typing import List
+from TTS.tts.configs.xtts_config import XttsConfig
+from TTS.tts.models.xtts import Xtts
+
+#torch.set_num_threads(int(os.environ.get("NUM_THREADS", os.cpu_count())))
+#device = torch.device("cuda" if os.environ.get("USE_CPU", "0") == "0" else "cpu")
+
+print("Loading xTTS...")
+config = XttsConfig()
+config.load_json("xtts_models/v2.0.2/config.json")
+model = Xtts.init_from_config(config)
+model.load_checkpoint(config, checkpoint_dir="xtts_models/v2.0.2/", use_deepspeed=True) #speaker file path?
+model.cuda()
+
+print("Computing speaker latents...")
+gpt_cond_latent, speaker_embedding = model.get_conditioning_latents(audio_path=["isa_sample.wav"])
+
+print("Running server...")
+
+app = FastAPI(
+    title="xtts streaming server",
+    docs_url="/"
+)
+
+def postprocess(wav):
+    """Post process the output waveform"""
+    if isinstance(wav, list):
+        wav = torch.cat(wav, dim=0)
+    wav = wav.clone().detach().cpu().numpy()
+    wav = wav[None, : int(wav.shape[0])]
+    wav = np.clip(wav, -1, 1)
+    wav = (wav * 32767).astype(np.int16)
+    return wav
+
+def encode_audio_common(
+    frame_input, encode_base64=True, sample_rate=24000, sample_width=2, channels=1
+):
+    """Return base64 encoded audio"""
+    wav_buf = io.BytesIO()
+    with wave.open(wav_buf, "wb") as vfout:
+        vfout.setnchannels(channels)
+        vfout.setsampwidth(sample_width)
+        vfout.setframerate(sample_rate)
+        vfout.writeframes(frame_input)
+
+    wav_buf.seek(0)
+    if encode_base64:
+        b64_encoded = base64.b64encode(wav_buf.getbuffer()).decode("utf-8")
+        return b64_encoded
+    else:
+        return wav_buf.read()
+    
+class StreamingInputs(BaseModel):
+    text: str
+    language: str = "en"
+    add_wav_header: bool = True
+    stream_chunk_size: str = "20"
+
+def predict_streaming_generator(parsed_input: dict = Body(...)):
+    # speaker_embedding = torch.tensor(parsed_input.speaker_embedding).unsqueeze(0).unsqueeze(-1)
+    # gpt_cond_latent = torch.tensor(parsed_input.gpt_cond_latent).reshape((-1, 1024)).unsqueeze(0)
+    text = parsed_input.text
+    language = parsed_input.language
+
+    stream_chunk_size = int(parsed_input.stream_chunk_size)
+    add_wav_header = parsed_input.add_wav_header
+
+
+    chunks = model.inference_stream(
+        text,
+        language,
+        gpt_cond_latent,
+        speaker_embedding,
+        stream_chunk_size=stream_chunk_size,
+        enable_text_splitting=True
+    )
+
+    for i, chunk in enumerate(chunks):
+        chunk = postprocess(chunk)
+        if i == 0 and add_wav_header:
+            yield encode_audio_common(b"", encode_base64=False)
+            yield chunk.tobytes()
+        else:
+            yield chunk.tobytes()
+
+@app.post("/tts_stream")
+def predict_streaming_endpoint(parsed_input: StreamingInputs):
+    return StreamingResponse(
+        predict_streaming_generator(parsed_input),
+        media_type="audio/wav",
+    )
---
+++ test_server_test.py
@@ -0,0 +1,80 @@
+import argparse
+import json
+import shutil
+import subprocess
+import sys
+import time
+from typing import Iterator
+
+import requests
+
+# def is_installed(lib_name: str) -> bool:
+#     lib = shutil.which(lib_name)
+#     if lib is None:
+#         return False
+#     return True
+
+
+# def save(audio: bytes, filename: str) -> None:
+#     with open(filename, "wb") as f:
+#         f.write(audio)
+
+
+def stream_ffplay(audio_stream):
+
+    ffplay_cmd = ["ffplay", "-nodisp", "-probesize", "1024", "-autoexit", "-"]
+
+
+    ffplay_proc = subprocess.Popen(ffplay_cmd, stdin=subprocess.PIPE)
+    for chunk in audio_stream:
+        if chunk is not None:
+            ffplay_proc.stdin.write(chunk)
+
+    # close on finish
+    ffplay_proc.stdin.close()
+    ffplay_proc.wait()
+
+
+def tts(text) -> Iterator[bytes]:
+    start = time.perf_counter()
+    speaker = dict()
+    speaker["text"] = text
+    speaker["language"] = "en"
+    speaker["stream_chunk_size"] = "20"  # you can reduce it to get faster response, but degrade quality
+    res = requests.post(
+        "http://localhost:8000/tts_stream",
+        json=speaker,
+        stream=True,
+    )
+    end = time.perf_counter()
+    print(f"Time to make POST: {end-start}s", file=sys.stderr)
+
+    if res.status_code != 200:
+        print("Error:", res.text)
+        sys.exit(1)
+
+    first = True
+    for chunk in res.iter_content(chunk_size=512):
+        if first:
+            end = time.perf_counter()
+            print(f"Time to first chunk: {end-start}s", file=sys.stderr)
+            first = False
+        if chunk:
+            yield chunk
+
+    print("⏱️ response.elapsed:", res.elapsed)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--text",
+        default="It took me quite a long time to develop a voice and now that I have it I am not going to be silent.",
+        help="text input for TTS"
+    )
+    args = parser.parse_args()
+    audio = stream_ffplay(
+        tts(
+            args.text
+        )
+    )
